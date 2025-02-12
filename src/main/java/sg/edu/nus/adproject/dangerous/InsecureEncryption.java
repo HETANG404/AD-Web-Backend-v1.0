1eb35d955543e28b00da1eaf748c466205378b85
BMonster77 <bmonsterzx@gmail.com>
2025-02-13 01:22:07 +0800

    zx-v0.90

--- src/main/java/sg/edu/nus/adproject/dangerous/InsecureEncryption.java
+++ src/main/java/sg/edu/nus/adproject/dangerous/InsecureEncryption.java
@@ -1,17 +1,17 @@
-package sg.edu.nus.adproject.dangerous;
-
-import javax.crypto.Cipher;
-import javax.crypto.spec.SecretKeySpec;
-
-public class InsecureEncryption {
-    public String encrypt(String password) {
-        try {
-            Cipher cipher = Cipher.getInstance("DES");  // 使用不安全的 DES 加密算法
-            cipher.init(Cipher.ENCRYPT_MODE, new SecretKeySpec("12345678".getBytes(), "DES"));
-            return new String(cipher.doFinal(password.getBytes()));
-        } catch (Exception e) {
-            e.printStackTrace();
-        }
-        return null;
-    }
-}
+//package sg.edu.nus.adproject.dangerous;
+//
+//import javax.crypto.Cipher;
+//import javax.crypto.spec.SecretKeySpec;
+//
+//public class InsecureEncryption {
+//    public String encrypt(String password) {
+//        try {
+//            Cipher cipher = Cipher.getInstance("DES");  // 使用不安全的 DES 加密算法
+//            cipher.init(Cipher.ENCRYPT_MODE, new SecretKeySpec("12345678".getBytes(), "DES"));
+//            return new String(cipher.doFinal(password.getBytes()));
+//        } catch (Exception e) {
+//            e.printStackTrace();
+//        }
+//        return null;
+//    }
+//}
